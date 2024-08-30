197cf0db8e595990ff808b5cad8dac5592b158d3
Dmitry Yako <dmtyak@gmail.com>
2024-08-30 12:28:20 +1200

    Fix tree test

--- app/_components/tree.test.tsx
+++ app/_components/tree.test.tsx
@@ -1,46 +1,57 @@
-import { afterEach, describe, expect, it } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
 import { cleanup, render } from "@testing-library/react";
 import type { DataNode } from "@/app/api/data";
 
 import Tree from "./tree";
 
+const treeData: DataNode[] = [
+  {
+    id: "1",
+    name: "Node 1",
+  },
+  {
+    id: "2",
+    name: "Node 2",
+  },
+  {
+    id: "3",
+    name: "Node 3",
+    children: [
+      {
+        id: "4",
+        name: "Node 4",
+      },
+      {
+        id: "5",
+        name: "Node 5",
+        children: [
+          {
+            id: "6",
+            name: "Node 6",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+vi.mock("react", async (importOriginal) => {
+  const actual = await importOriginal()
+  return {
+    ...actual as object,
+    use: () => treeData,
+  }
+})
+
 describe("Tree Component", () => {
   afterEach(() => {
     cleanup();
   });
-  const treeData: DataNode[] = [
-    {
-      id: "1",
-      name: "Node 1",
-    },
-    {
-      id: "2",
-      name: "Node 2",
-    },
-    {
-      id: "3",
-      name: "Node 3",
-      children: [
-        {
-          id: "4",
-          name: "Node 4",
-        },
-        {
-          id: "5",
-          name: "Node 5",
-          children: [
-            {
-              id: "6",
-              name: "Node 6",
-            },
-          ],
-        },
-      ],
-    },
-  ];
 
   it("should be able to render a node array", () => {
-    const { getAllByText } = render(<Tree data={treeData} />);
+    const { getAllByText } = render(
+      <Tree dataRequest={Promise.resolve(treeData)} />,
+    );
     expect(getAllByText("Node", { exact: false }).length).toBe(3);
   });
 });
